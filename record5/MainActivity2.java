4f68b3d95ca6b7d9552cc86066db9b43e5140d2c
aswani26 <aswania9365@gmail.com>
2026-07-29 15:09:21 +0530

    Add MainActivity2 to handle user welcome message

---
+++ record5/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.sharedpreferences;
+
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitymain2);
+        t1= findViewById(R.id.textView);
+
+        String name = getIntent().getStringExtra("username");
+        t1.setText("welcome"+" " +name);
+
+    }
+
+
+
+
+}
